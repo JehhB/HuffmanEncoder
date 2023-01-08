12b98325d1f920b532cab2fd453be2656c2fd47a
JehhB <jwy.bayod@gmail.com>
2023-01-08 09:04:04 +0800

    Initialize project

---
+++ src/huffmanencode/HuffmanEncode.java
@@ -0,0 +1,12 @@
+package huffmanencode;
+
+public class HuffmanEncode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
